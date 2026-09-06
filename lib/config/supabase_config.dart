64e56e257b75225f47d486257cad17fa41d364e8
alvinandakurniawann <alvinanda2004@gmail.com>
2026-09-06 14:15:48 +0700

    pindah database ke project supabase baru

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -1,5 +1,5 @@
 class SupabaseConfig {
-  static const String url = 'https://zndtcyckdcqdsnfyflnw.supabase.co';
+  static const String url = 'https://uwjocqkksautlopbnknj.supabase.co';
   static const String anonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpuZHRjeWNrZGNxZHNuZnlmbG53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwNzEyNzEsImV4cCI6MjA1ODY0NzI3MX0.jnr27p01G07BCoDNNFQ41Ov9AaEqSha5vCIl4MxUK1Q';
+      'sb_publishable_p2FhLmnKHt5mCJ1xdY9Seg_uB0Xa6r8';
 }
